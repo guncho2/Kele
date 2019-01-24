aa561743aa0444998054f04bd9ef745d5afd51a3
Eduardo Lowensohn <gunchosoft@gmail.com>
2019-01-24 00:39:27 -0300

    iniauthassignm3

--- lib/kele.rb
+++ lib/kele.rb
@@ -11,12 +11,10 @@ class Kele
     def initialize(email, password)
         response = self.class.post("https://private-amnesiac-822888-blocapi.apiary-proxy.com/api/v1/sessions", body: { 'email': email, 'password': password })
         if response && response["auth_token"]
-     @auth_token = response["eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcGlfa2V5IjoiMGY2ZTg2YzE2YTYxNDg0MDk1MmE3ZjgxZTlkNmQ0OGUiLCJ1c2VyX2lkIjoyMzk5ODg2LCJuYW1lIjoiRWR1YXJkbyBMb3dlbnNvaG4ifQ.rX65Nwcpwl-CRSyjGKkPjSm6puoA6aNqio3p7y-Y0qw"]
+     @auth_token = response['auth_token']
      puts "#{email} is sucessfully in with auth_token #{@auth_token}"
    else
      puts "Login invalid"
    end
- end
-
-
+end
 end
