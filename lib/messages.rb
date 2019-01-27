1cfc53ee3903b7a0d1d25d606408d7f18ab70e52
Eduardo Lowensohn <gunchosoft@gmail.com>
2019-01-27 18:30:08 -0300

    1

---
+++ lib/messages.rb
@@ -0,0 +1,36 @@
+require 'httparty'
+require 'jason'
+
+module Messages
+  include HTTParty
+
+  BASE_URI = 'https://www.bloc.io/api/v1'
+
+
+  def get_messages(page_number = 1)
+    response = self.class.get("#{BASE_URI}/message_threads"), headers: { "authroization" => @auth_token },
+    body: {
+      "page" = page_number
+
+    })
+
+    puts "This is page #{page_number} of your messages" if response.success?
+
+    JSON.parse(response.body)
+
+  end
+
+  def create_message(sender, recipient_id, token, subject, stripped-text)
+    response = self.class.post("#{BASE_URI}/messages"), headers: { "authorization" => @auth_token },
+    body: {
+      "sender" => sender,
+      "recipient_id" => recipient_id,
+      "token" => token,
+      "subject" => subject,
+      "stripped-text" => text
+
+    })
+    puts "Your messages was sent!" if response.success?
+
+  end
+end
