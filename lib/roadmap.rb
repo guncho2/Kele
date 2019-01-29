ee5838d5cc6f92ab24bfbd19fc78df509d078b42
Eduardo Lowensohn <gunchosoft@gmail.com>
2019-01-29 12:43:57 -0300

    1

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -21,4 +21,10 @@ module Roadmap
     )
     JSON.parse(response.body)
   end
+
+  def get_remaining_checkpoints(chain_id)
+    response = self.class.get("#{BASE_URI}/enrollment_chains/#{chain_id}/checkpoints_remaining_in_section"), headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+  
 end
